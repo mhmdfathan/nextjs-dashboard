4ceade92ab1b3bfc69fe46e2af348879258cf92e
fat <mhmdfathan007@gmail.com>
2025-02-23 23:14:05 +0700

    comlete

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from "zod";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import postgres from "postgres";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 
 export type State = {
   errors?: {
@@ -78,7 +80,7 @@ export async function updateInvoice(
       message: "Missing Fields. Failed to Update Invoice.",
     };
   }
-  // 
+  //
   const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
 
@@ -97,8 +99,27 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error("Failed to Delete invoice");
+  // throw new Error("Failed to Delete invoice");
 
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
 }
